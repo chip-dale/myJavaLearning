6d9adb827c18ecd5ec63e931ded4b71dcc11f0c7
Gadget Hackwrench <62562443+chip-dale@users.noreply.github.com>
2021-04-02 05:45:57 +0400

    Add files via upload

---
+++ Lesson06/Task1.java
@@ -0,0 +1,53 @@
+/*
+Fidan Ismailova
+
+    Калькулятор с операциями *, +, -, /, %
+Ввод х = 2 Вывод 2 + 3 = 5
+     y = 3
+     знак +
+ */
+import java.util.Scanner;
+
+public class Lesson6Task1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\t{CALCULATOR}");
+
+        double x = 0;
+        double y = 0;
+        char symbol = 0;
+        String exit = "\t{Enter Operation >>  q  >> if you want EXIT}";
+        while (symbol != 'q') {
+            System.out.print("Enter First Number >> ");
+            x = sc.nextInt();
+            System.out.print("Enter Second Number >> ");
+            y = sc.nextInt();
+            System.out.print("Enter Operation >>\n\t+  -  *  /  % \t");
+            symbol = sc.next().charAt(0);
+
+            double result = 0;
+            if (symbol == '+') {
+                result = x + y;
+                System.out.println("=>\n" + x + " + " + y + " = " + result);
+                System.out.println(exit);
+            } else if (symbol == '-') {
+                result = x - y;
+                System.out.println("=>\n" + x + " - " + y + " = " + result);
+                System.out.println(exit);
+            } else if (symbol == '*') {
+                result = x * y;
+                System.out.println("=>\n" + x + " * " + y + " = " + result);
+                System.out.println(exit);
+            } else if (symbol == '/') {
+                result = x / y;
+                System.out.println("=>\n" + x + " / " + y + " = " + result);
+                System.out.println(exit);
+            } else if (symbol == '%') {
+                result = x % y;
+                System.out.println("=>\n" + x + " % " + y + " = " + result);
+                System.out.println(exit);
+            }
+        }
+    }
+}
