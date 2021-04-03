325410bb54f788346d2c18fdd58527e703ae50be
Gadget Hackwrench <62562443+chip-dale@users.noreply.github.com>
2021-04-04 00:28:03 +0400

    Update and rename Task1.java to MinAndMaxOfArray.java

--- Lesson07/MinAndMaxOfArray.java
+++ Lesson07/MinAndMaxOfArray.java
@@ -3,7 +3,7 @@
  */
 import java.util.Scanner;
 
-public class Task1 {
+public class MinAndMaxOfArray {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
 
