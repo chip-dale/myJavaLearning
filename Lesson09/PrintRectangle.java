fd5cca91cf1f80b887568a3f25694f5c240d5ef2
Gadget Hackwrench <62562443+chip-dale@users.noreply.github.com>
2021-04-04 00:23:49 +0400

    Update and rename Task3.java to PrintRectangle.java

--- Lesson09/PrintRectangle.java
+++ Lesson09/PrintRectangle.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
     *  *
     ****
 */
-public class Task3 {
+public class PrintRectangle {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
 
