afa333d6771b4ccfd7ac54f46190ab92c233c210
Gadget Hackwrench <62562443+chip-dale@users.noreply.github.com>
2021-04-04 00:04:50 +0400

    Update and rename Task3.java to AbsoluteValueOfNum.java

--- Lesson05/AbsoluteValueOfNum.java
+++ Lesson05/AbsoluteValueOfNum.java
@@ -5,7 +5,7 @@
  */
 import java.util.Scanner;
 
-public class Task3 {
+public class AbsoluteValueOfNum {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("PLEASE ENTER NUMBER:");
