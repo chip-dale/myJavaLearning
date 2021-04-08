2681aaa39c6d19725ff2e54e0f099f3241706f5c
Gadget Hackwrench <62562443+chip-dale@users.noreply.github.com>
2021-04-09 03:50:48 +0400

    Update Task1.java

--- Lesson27/Task1.java
+++ Lesson27/Task1.java
@@ -1,7 +1,7 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * Создать класс customer c полями id, name, surname.
@@ -10,13 +10,19 @@ import java.util.stream.IntStream;
  */
 public class Task1 {
     public static void main(String[] args) {
-        List<Customer> customerList = IntStream.rangeClosed(1, 4)
-                .mapToObj(Customer :: new)
-                .collect(Collectors.toList());
+
+        int id = 0;
+        List<Customer> customers = Arrays.asList(
+                new Customer(++id,"Кролик","Белый"),
+                new Customer(++id,"Кот","Чеширский"),
+                new Customer(++id,"Заяц","Мартовский"),
+                new Customer(++id,"Шляпник","Безумный"),
+                new Customer(++id,"Соня","Мышь")
+        );
+        System.out.println(customers);
 
         Map<String, Customer> map =
-                customerList.stream().collect(Collectors.toMap(Customer :: getKey, item -> item));
-
+                customers.stream().collect(Collectors.toMap(Customer :: getKey, value -> value));
         map.forEach((k, v) -> System.out.println(k + " => " + v));
     }
 }
@@ -26,11 +32,8 @@ class Customer {
     private String name;
     private String surname;
 
-    Customer(int id) {
+    Customer(int id, String name, String surname) {
         this.id = id;
-    }
-
-    Customer(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
@@ -39,7 +42,17 @@ class Customer {
         return "Key-" + id;
     }
 
-    public String getValue(){
-        return "Value: " + name + surname;
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
     }
 }
+
+//  https://ru.stackoverflow.com/questions/765563/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA-%D0%B8-%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2-%D0%B8%D0%B7-map-%D0%B2-list-java-stream-api
+//  https://coderoad.ru/52126290/Java-8-%D0%BF%D0%BE%D1%82%D0%BE%D0%BA-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B0-%D0%B8%D0%B7-%D0%BA%D0%B0%D1%80%D1%82%D1%8B-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2
+//  https://annimon.com/article/2778
+//  https://overcoder.net/q/32660/java-%D0%BA%D0%B0%D0%BA-%D0%BF%D1%80%D0%B5%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D0%B2-%D0%BA%D0%B0%D1%80%D1%82%D1%83
